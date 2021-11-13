e602d22f41ee1448b04fdf7c395231957410e1ee
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-11-13 13:08:13 +0900

    fix type error

--- packages/server/src/controller/book/[bookId]/word/[wordId]/comment/[commentId]/delete/db.ts
+++ packages/server/src/controller/book/[bookId]/word/[wordId]/comment/[commentId]/delete/db.ts
@@ -1,4 +1,4 @@
-import prisma from "../../../../../../lib/prisma"
+import prisma from "../../../../../../../../lib/prisma"
 
 const deleteComment = async (commentId: string) => {
   await prisma.comment.update({
