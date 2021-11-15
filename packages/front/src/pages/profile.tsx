df056f0dca45a4590ee81b215a5e29c21f43aa51
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-11-15 23:21:52 +0900

    remvoe unused file

--- packages/front/src/pages/profile.tsx
+++ packages/front/src/pages/profile.tsx
@@ -2,13 +2,13 @@ import { NextPage } from "next"
 import Link from "next/link"
 import React from "react"
 import useSWR from "swr"
-import { useApiClient } from "../provider/ApiClientProvider"
+// import { useApiClient } from "../provider/ApiClientProvider"
 import { useAuthUser } from "../provider/LiffProvider"
 
 const Test: NextPage = () => {
-  const apiClient = useApiClient()
-  const { data, error } = useSWR(apiClient.token == null ? null : "/owned-book", () => apiClient.get("/owned-book"))
-  const authUser = useAuthUser()
+  // const apiClient = useApiClient()
+  // const { data, error } = useSWR(apiClient.token == null ? null : "/owned-book", () => apiClient.get("/owned-book"))
+  // const authUser = useAuthUser()
 
   return (
     <div>
@@ -17,13 +17,13 @@ const Test: NextPage = () => {
           <a>HOME</a>
         </Link>
       </p>
-      <p>name: {authUser?.name}</p>
+      {/* <p>name: {authUser?.name}</p>
       <p>token: {authUser?.token}</p>
       {authUser?.iconUrl != null && <img width="400px" src={authUser?.iconUrl} alt="" />}
       <code>{JSON.stringify(data?.data)}</code>
       {data?.data.ownedBooks?.map((book) => (
         <p key={book.id}>{book.book.title}</p>
-      ))}
+      ))} */}
     </div>
   )
 }
